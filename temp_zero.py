3b5458f7bc6a22f2d93a244dc4da41aff195b1f0
Hang Phung <hang.phungthu01@gmail.com>
2025-04-08 00:54:47 +0700

    Add the overall notebook - perfomance comparison and class influence

--- temp_zero.py
+++ temp_zero.py
@@ -22,6 +22,7 @@ text_inputs = torch.cat([tokenizer(f"a photo of a {c}") for c in cifar10.classes
 
 
 with torch.no_grad():
+    # import pdb; pdb.set_trace()
     image_features = model.encode_image(image_input)
     text_features = model.encode_text(text_inputs)
     image_features /= image_features.norm(dim=-1, keepdim=True)
@@ -29,6 +30,8 @@ with torch.no_grad():
 
 similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
 values, indices = similarity[0].topk(5)
+import pdb; pdb.set_trace()
+top_1_accuracy = np.mean((test_labels == class_id).astype(float)) * 100.
 
 # Print the result
 print("\nTop predictions:\n")
